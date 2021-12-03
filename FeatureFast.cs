c82e7caf119bf109160caeccbd34b8a3c3e9b0f4
Sergey <ser200026@mail.ru>
2021-12-03 22:38:17 +0700

    Question5

---
+++ FeatureFast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForTestLect10
+{
+    class Program
+    {
+        static double FeatureFast(double  x, int n)
+        {
+            var result = 1.0;
+            while (n > 0)
+            {
+                if ((n & 1) == 0)
+                {
+                    x *= x;
+                    n >>= 1;
+                }
+                else
+                {
+                    result *= x;
+                    --n;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double value;
+            int degree;
+            value = double.Parse(Console.ReadLine());
+            degree = int.Parse(Console.ReadLine());
+            Console.WriteLine(FeatureFast(value, degree));
+        }
+    }
+}
