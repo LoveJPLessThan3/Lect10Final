2f17addf107c0b0cfe722620d2983259ad132c9a
Sergey <ser200026@mail.ru>
2021-12-03 22:48:51 +0700

    Question7

---
+++ BugFix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForTestLect10
+{
+    class Program
+    {
+        static double MyPow(double value, int degree)
+        {
+            double temp = value;
+            while (degree > 1)
+            {
+                temp *= value;
+                degree--;
+            }
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            double value;
+            int degree;
+            Console.Write("Введите число");
+            value = double.Parse(Console.ReadLine());
+            Console.Write("Введите степень");
+            degree = int.Parse(Console.ReadLine());
+            Console.WriteLine(MyPow(value, degree));
+        }
+    }
+}
