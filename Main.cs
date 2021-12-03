8daec19c0fb7ff7a5837b310bfbdc469a1566f0b
Sergey <ser200026@mail.ru>
2021-12-03 22:24:04 +0700

    Version1.0

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForTestLect10
+{
+    class Program
+    {
+        static double MyPow(double value, int degree)
+        {
+            double temp = value;
+            while (degree > 1)
+            {
+                temp *= value;
+                degree--;
+            }
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            double value;
+            int degree;
+            value = double.Parse(Console.ReadLine());
+            degree = int.Parse(Console.ReadLine());
+            Console.WriteLine(MyPow(value, degree));
+        }
+    }
+}
